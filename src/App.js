0545c0d58c74d2390b2f97c6d6d8daf7c7e9b060
Sahil <vermasahil829@gmail.com>
2021-04-16 16:26:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpenses/NewExpense';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const expenses = [
+		{
+			id: 'e1',
+			title: 'Tishu Paper',
+			amount: 94.12,
+			date: new Date(2021, 2, 14),
+		},
+		{ id: 'e2', title: 'New TV', amount: 799.42, date: new Date(2021, 1, 11) },
+		{
+			id: 'e3',
+			title: 'Car Insurance',
+			amount: 294.12,
+			date: new Date(2021, 3, 19),
+		},
+		{ id: 'e4', title: 'New Desk', amount: 450, date: new Date(2021, 2, 23) },
+	];
+
+	// return React.createElement(
+	// 	'div',
+	// 	{},
+	// 	React.createElement('h2', {}, 'Hello'),
+	// 	React.createElement(Expenses, { items: expenses })
+	// );
+	const addExpenseHandler = (expense) => {
+		console.log(expense);
+	};
+
+	return (
+		<div className='App'>
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
+		</div>
+	);
 }
 
 export default App;
