3863ac67665ee551a44578e082f104d823e2ae89
SahilVerma829 <vermasahil829@gmail.com>
2021-04-19 18:57:03 +0530

    ui update

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <div className='App bg-light'>
       <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
